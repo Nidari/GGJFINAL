95f07f493b990fbb8ba978baaa19a0233b5ba92e
MSI\Kla <boongie123@gmail.com>
2017-01-21 09:05:42 +0100

    commander camera lerp

---
+++ Assets/CommandoCameraLerp.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CommandoCameraLerp : MonoBehaviour
+{
+    bool cameraToMove;
+    public Transform nextRoomTr;
+    private Vector3 startPos;
+    float timer = 0;
+    Transform cameraTr;
+    // Use this for initialization
+    void OnTriggerEnter (Collider coll)
+    {
+    
+        cameraTr = GameObject.FindGameObjectWithTag("CommanderCam").transform;
+        startPos = cameraTr.position;
+        cameraToMove = true;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (cameraToMove)
+        {
+            timer += Time.deltaTime;
+            cameraTr.position = Vector3.Lerp(startPos, nextRoomTr.position, timer);
+        }
+	}
+}
