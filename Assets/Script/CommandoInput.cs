9c5d076a4a7616ae2b638b45de1ab67e23732437
Riccardo Cambiaghi <riccardocambiaghi@yahoo.it>
2017-01-21 10:46:59 +0100

    inserito cooldownd al commander

--- Assets/Script/CommandoInput.cs
+++ Assets/Script/CommandoInput.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class CommandoInput : MonoBehaviour {
 
     public Camera commandoCamera;
-
+    public bool canUsePower = true;
+    public float countdown = 2;
+    private Coroutine cooldownCoroutine;
     // Use this for initialization
     void Start()
     {
@@ -14,7 +16,7 @@ public class CommandoInput : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButton("Fire1Commando"))
+        if (Input.GetButton("Fire1Commando") && canUsePower)
         {
             commandoCamera.cullingMask |= (1 << LayerMask.NameToLayer("CommanderOnly"));
         }
@@ -22,10 +24,27 @@ public class CommandoInput : MonoBehaviour {
         {
             commandoCamera.cullingMask &= ~(1 << LayerMask.NameToLayer("CommanderOnly"));
         }
+        if (Input.GetButtonUp("Fire1Commando")) 
+        {
+           cooldownCoroutine = StartCoroutine(CooldownPowerCommanderCO());
+            canUsePower = false;
+        }
         if (Input.GetButton("Fire1"))
         {
             Debug.Log("sono il player coglione");
         }
 	}
+
+    IEnumerator CooldownPowerCommanderCO()
+    {
+        float timer = countdown;
+        while(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            yield return null;
+        }
+        canUsePower = true;
+        StopAllCoroutines();
+    }
 }
 //~
