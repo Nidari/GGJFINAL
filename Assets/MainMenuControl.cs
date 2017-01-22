a2aba3cbe5e6c3ced3c8688c3c4b9bb4ce36015a
DESKTOP-85V8C1V\Tommaso <tommaso.bassini@gmail.com>
2017-01-22 01:47:43 +0100

    menu principale

---
+++ Assets/MainMenuControl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public float animationSpeed;
+    public Sprite[] spriteFrames;
+    public GameObject[] mainMenuButton;
+
+	void Start ()
+    {
+        StartCoroutine(PlayAnim());	
+	}
+	
+    IEnumerator PlayAnim()
+    {
+        Image image = GetComponent<Image>();
+
+        for (int i = 0; i < spriteFrames.Length; i++)
+        {
+            image.sprite = spriteFrames[i];
+            yield return new WaitForSeconds(animationSpeed);
+        }
+
+        foreach (var button in mainMenuButton)
+        {
+            button.SetActive(true);
+        }
+
+        mainMenuButton[0].GetComponent<Button>().Select();
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+
+    public void Credit()
+    {
+        SceneManager.LoadScene("Credit");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
